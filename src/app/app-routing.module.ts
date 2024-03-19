4eaf100e88cc361f87fd5d97bc68ee32d351be5f
Jan Armando Rodriguez Mejia <99633762+JantXz@users.noreply.github.com>
2024-03-18 23:49:41 -0600

    Formulario cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { PrincipalComponent } from './components/principal/principal.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 const routes: Routes = [
   {path: 'nav/:id', component: NavComponent},
   {path: '', redirectTo: 'inicio', pathMatch: 'full'},
   {path: 'inicio', component: InicioComponent},
+  {path: 'formulario', component: FormularioComponent},
   {path: 'Principal', component: PrincipalComponent},
 ];
 
